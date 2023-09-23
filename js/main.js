a27cab6d75d6fd728f5d544b32073318f91093c7
Jiří Patejdl <jiri.patejdl@gmail.com>
2023-09-23 12:43:51 +0200

    das

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const dialog = document.querySelector("dialog")
+
+
+
+const openWineList = document.querySelector("#openWineList")
+openWineList.addEventListener("click", () => {
+  dialog.showModal()
+})
+
+const closeWineList = document.querySelector("#closeWineList")
+closeWineList.addEventListener("click", () => {
+  dialog.close()
+})
+dialog.addEventListener("click", e => {
+    const dialogDimensions = dialog.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      dialog.close()
+    }
+  })
